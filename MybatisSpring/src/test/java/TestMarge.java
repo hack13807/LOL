bdf1760be897a208c7277264486e9963091dc37b
wells <490864627@qq.com>
2019-09-24 20:39:42 +0800

    新增子模块

---
+++ MybatisSpring/src/test/java/TestMarge.java
@@ -0,0 +1,9 @@
+import com.java.lol.piece.Ali;
+import com.java.lol.piece.Piece;
+
+public class TestMarge {
+
+    public static void main(String[] args) {
+        Piece piece = new Ali();
+    }
+}
